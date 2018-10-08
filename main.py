f837f5b7be5634387297e64deb57e75d96c3fdfa
Matheus_DeLima <matheuslimaaa13@gmail.com>
2018-10-08 11:22:23 -0300

    teste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
 from controlers import routes
 app = Flask(__name__)
 app.register_blueprint(routes.rotas)
 
-# app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
