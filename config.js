7e25f3e28dd73b1bb08cc31f5138e6c42b548aaf
Matheus_DeLima <matheuslimaaa13@gmail.com>
2019-01-31 16:26:32 -0200

    banco

--- config.js
+++ config.js
@@ -3,11 +3,19 @@ module.exports = {
     secret: "*RJ%bRAkr#dK?&f9JEmZXKP@?H68kAv7Hj^&3Hh6nLjM*8db#z",
     port: port,
     base_url: "http://127.0.0.1:" + port,
+    // bd: {
+    //     connectionLimit: 20,
+    //     host: "127.0.0.1",
+    //     user: "root",
+    //     password: "root",
+    //     database: "rodovias"
+    // }
     bd: {
-        connectionLimit: 20,
-        host: "127.0.0.1",
-        user: "root",
-        password: "root",
-        database: "rodovias"
+        connectionLimit: 10,
+        host: "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "nynyxn7w891pahsa",
+        password: "zij7zp7igsbaxtil",
+        database: "c8ng80padace9x3w"
     }
+
 }
