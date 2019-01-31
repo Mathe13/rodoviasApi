35c271542488bbb36274356985934be305bb69be
Matheus_DeLima <matheuslimaaa13@gmail.com>
2019-01-31 17:27:29 -0200

    banco funcional
    so funciona no jawsdb via string

--- services/mysql_db.js
+++ services/mysql_db.js
@@ -1,16 +1,20 @@
 const mysql = require("mysql")
 const config = require("../config")
 
-var con = mysql.createPool({
-    // host: "149.56.100.42",
-    // user: "root",
-    // password: "xy_a7mpJkCj_",
-    // database: "app_instalador",
-    connectionLimit: 20,
-    host: config.bd.host,
-    user: config.bd.user,
-    password: config.bd.user,
-    database: config.bd.database
-});
+var con = mysql.createPool(
+    'mysql://nynyxn7w891pahsa:zij7zp7igsbaxtil@edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c8ng80padace9x3w'
+
+    // {
+    //     // host: "149.56.100.42",
+    //     // user: "root",
+    //     // password: "xy_a7mpJkCj_",
+    //     // database: "app_instalador",
+    //     connectionLimit: 20,
+    //     host: config.bd.host,
+    //     user: config.bd.user,
+    //     password: config.bd.user,
+    //     database: config.bd.database
+    // }
+);
 // con.connect();
 module.exports = con
