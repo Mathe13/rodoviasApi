d8fe082c17e5f2de3aa030fba6bac6e00e03b767
Matheus_DeLima <matheuslimaaa13@gmail.com>
2019-02-04 16:25:33 -0200

    teste

--- src/sensor/sensor_routes.js
+++ src/sensor/sensor_routes.js
@@ -29,6 +29,7 @@ router.get("/:sensor", function (req, res) {
 
 router.post("/:sensor", function (req, res) {
     // let sensor = "acelerometro"
+    console.log(req.body.datahora)
     req.body.datahora = (fecha.format(req.body.datahora, 'YYYY-MM-DD HH:mm:ss'))
     if (["acelerometro", "giroscopio", "gps"].includes(req.params.sensor)) {
         sensor.insert(req.params.sensor, req.body)
